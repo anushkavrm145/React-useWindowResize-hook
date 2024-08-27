932142e015f512d969cf119c34fce791ce91b91a
anushkavrm145 <anushkavrm145@gmail.com>
2024-08-27 12:00:20 +0530

    first

---
+++ src/components/test.jsx
@@ -0,0 +1,12 @@
+import useWindowResize from "."
+
+export default function useWindowResizeTest(){
+    const windowSize = useWindowResize();
+    const {width,height} = windowSize;
+    return( <div>
+        <h1>use window resize hook</h1>
+        <p>width is {width}</p>
+        <p>height is {height}</p>
+    </div>
+    );
+}
